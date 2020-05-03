658ee5e2d34dd83d2f276915650b85e0fd03e06a
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-03 14:57:53 -0400

    1 file modified: *adds typescript for toolbar input

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
 
+export class TopBarComponent implements OnInit {
+email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
 
