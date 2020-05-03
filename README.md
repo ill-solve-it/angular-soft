7885b9c1fdf3ab05286b31dfc79e0c252071aca4
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-03 13:01:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-soft
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-soft)
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MDBBootstrapModule, DropdownModule, InputsModule, CardsModule } from 'angular-bootstrap-md';
+import { MatSliderModule } from '@angular/material/slider';
+import {  } from '@angular/material/slider';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  imports: [
+    
+    MatSliderModule,
+    CardsModule,
+    InputsModule,
+    DropdownModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  
+  declarations: [
+
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
